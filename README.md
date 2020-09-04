5a7b506c445133a64de48dc58c0ca315328890b7
art-litv <70752711+art-litv@users.noreply.github.com>
2020-09-04 23:24:09 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,54 @@
+Bitlink converter
+=================
+
+Bitlink converter is an easy script wrote in Python which converts links to bitlinks.
+
+Bitlink is a link which leads you to a webpage instead of its original link.
+
+For example, both `bit.ly/111` and `https://www.google.com/` lead to google search page.
+
+The script simply uses API to convert a link to a bitlink
+
+Usage
+-----
+-----
+
+With [Bitly](https://bitly.com/) you can create bitlinks by using API
+
+You can use bitlinks for SMM to make a shorter link for your website. With this you can increase a number of clients coming on your website. You can read more about why you should use bitlinks [here](https://bitly.com/pages/why-bitly/bitly-101)
+
+This Python script has the following features:
+if you insert a simple link you will get a bitlink. If you insert a bitlink you will get the number of times this bitlink has been clicked.
+
+Installing
+----------
+----------
+
+To use this Python script you will need 2 libraries: requests and python-dotenv. You can install them using `pip`.
+
+Place repository in any directory and write the path to the repository through cmd:
+
+    cd <PATH>
+
+Then:
+
+    pip install -r requirements.txt
+
+Now you can use the script through cmd. Insert your link instead of `<link>`:
+
+    python main.py <link>
+
+Examples
+----------
+----------
+
+`<link>` as simple link:
+
+    C:\LinkToBitlink>python main.py https://github.com/
+    Bitlink bit.ly/2DAwChO
+
+`<link>` as bitlink
+
+    C:\LinkToBitlink>python main.py bit.ly/2DAwChO
+    Sum of clicks on the bitlink 0
+
---
+++ main.py
@@ -0,0 +1,61 @@
+import requests
+from dotenv import load_dotenv
+import os
+import argparse
+
+load_dotenv()
+
+HEADERS = {
+  'Authorization': f'Bearer {os.getenv("BITLY_TOKEN")}'
+}
+
+
+def shorten_link(headers, long_url, group_guid=None, domain=None):
+  '''Returns bitlink from long link'''
+  payload = {
+    'long_url': long_url,
+    'group_guid': group_guid,
+    'domain': domain
+  }
+  response = requests.post('https://api-ssl.bitly.com/v4/shorten', headers=headers, json=payload)
+  response.raise_for_status()
+  return response.json()['link'].strip('https://')
+
+
+def count_bitlink_clicks(headers, bitlink, unit="day", units=-1):
+  '''Returns sum of clicks on bitlink'''
+  payload = {
+    'unit': unit,
+    'units': units
+  }
+  response = requests.get(
+    f'https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary',
+    headers=headers,
+    params=payload
+    )
+  response.raise_for_status()
+  return response.json()['total_clicks']
+
+
+def main():
+  parser = argparse.ArgumentParser()
+  parser.add_argument("link", help="link to create a bitlink or bitlink to see sum of clicks")
+  url = parser.parse_args().link
+  if url.startswith("bit.ly/"):
+    clicks_count = None
+    try:
+      clicks_count = count_bitlink_clicks(HEADERS, url)
+      print('Sum of clicks on the bitlink', clicks_count)
+    except requests.exceptions.HTTPError:
+      print("Your link or token is not correct")
+  else:
+    bitlink = None
+    try:
+      bitlink = shorten_link(HEADERS, url)
+      print('Bitlink', bitlink)
+    except requests.exceptions.HTTPError:
+      print("Your link or token is not correct")
+
+
+if __name__ == '__main__':
+  main()
